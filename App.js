9fbe84a666798a3d1a63c58503cc8dc780b75340
lucdejems <luc.dejems@gmail.com>
2018-10-16 11:38:39 +0200

    added app files

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Aucun article</Text>
+    <Button>Ajouter un article</Button>
+  </View>
+);
+
+export default App;
