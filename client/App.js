c6cd61e755bab1cd937c645f9764472905406c35
lucdejems <luc.dejems@gmail.com>
2018-11-14 15:24:36 +0100

    discard button from app.js

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,7 +13,6 @@ const styles = StyleSheet.create({
 const App = () => (
   <View style={styles.container}>
     <Text>Aucun article</Text>
-    <Button>Ajouter un article</Button>
   </View>
 );
 
