5e92011884a7d162e48c2516a828816eaa0b710f
Helene Blanchard <erba0614@gmail.com>
2018-12-12 10:32:20 +0100

    sauvegarde de mes changements

--- client/services/ArticleService.js
+++ client/services/ArticleService.js
@@ -4,5 +4,9 @@ function getArticle() {
   return axios.get('https://wcs-projet2.herokuapp.com/articles');
 }
 
+function postArticle() {
+  return axios.post('https://wcs-projet2.herokuapp.com/articles');
+}
+
 // eslint-disable-next-line
-export { getArticle };
+export { getArticle, postArticle };
--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
 
+
+const { getDocumentData } = require("./scrape");
+
+const saveArticle = async (req, res) => {
+  const url = req.body.url;
+  const documentData = await getDocumentData(url)
+  await axios
+  .post('https://wcs-projet2.herokuapp.com/articles', {
+   articles
+  })
+  //.then( response => {
+  //  console.log(response.data);
+  //})
+  .catch( error => {
+    console.log(error);
+  });   
+   
+
+  res.status(201);
+  res.send({ articles: documentData });
+  
+  };
+
+const app = express();
+
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .post("/articles", saveArticle)
+  .listen(3004, () => console.log("server, listening on port 3004"));
+  
