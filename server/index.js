4cf92096de3b5e3cc1c39003f1d995f8216495c7
Helene Blanchard <erba0614@gmail.com>
2018-12-13 10:30:49 +0100

    sauvegarde de mes changements

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const { getDocumentData } = require("./scrape");
+
+var database = firebase.database();
+
+function writeUserData(Ujm5AV0Y7uqLjpJxbKns, myTitle, myDescription) {
+  firebase.database().ref('articles/' + Ujm5AV0Y7uqLjpJxbKns).set({
+    title: myTitle,
+    description: myDescription
+  });
+}
 
 const saveArticle = async (req, res) => {
   const url = req.body.url;
   const documentData = await getDocumentData(url);
 
   //TODO: save to database
+  
+
+  'https://wcs-projet-2.firebaseio.com/message_list.json'
+  
   res.status(201);
   res.send({ articles: documentData });
 };
@@ -15,6 +30,6 @@ const app = express();
 app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
-  .post("/articles", saveArticle)
+  .post("/articles", saveArticle, writeUserData)
   .listen(3004, () => console.log("server, listening on port 3004"));
   
