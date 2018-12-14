553f6592c83679b1786493a886df5aca89200b5b
Helene Blanchard <erba0614@gmail.com>
2018-12-14 11:56:29 +0100

    post url

--- server/index.js
+++ server/index.js
@@ -1,35 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const admin = require("firebase-admin");
 const { getDocumentData } = require("./scrape");
 
-var database = firebase.database();
+const serviceAccount = require("./wcs-projet-2-firebase-adminsdk-5e2k5-e90f0ccb6f.json");
 
-function writeUserData(Ujm5AV0Y7uqLjpJxbKns, myTitle, myDescription) {
-  firebase.database().ref('articles/' + Ujm5AV0Y7uqLjpJxbKns).set({
-    title: myTitle,
-    description: myDescription
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://wcs-projet-2.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+function writeArticle(url) {
+  return getDocumentData(url).then(data => {
+    console.log(data);
+    db.collection("articles")
+      .add({
+        title: data.title,
+        description: data.description,
+        thumbnail: data.thumbnail,
+        source: data.source
+      })
+      .then(ref => {
+        console.log("Added document with ID: ", ref.id);
+      });
   });
 }
-
-const saveArticle = async (req, res) => {
-  const url = req.body.url;
-  const documentData = await getDocumentData(url);
-
-  //TODO: save to database
-  
-
-  'https://wcs-projet-2.firebaseio.com/message_list.json'
-  
-  res.status(201);
-  res.send({ articles: documentData });
-};
 
 const app = express();
 
 app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
-  .post("/articles", saveArticle, writeUserData)
+  .get("/", (req, res) => {
+    res.send("Hello world");
+  })
+  .post("/articles", (req, res) => {
+    console.log(req.body);
+    const url = req.body.url;
+    writeArticle(url)
+      .then(() => {
+       res.sendStatus(201);
+    });
+  })
   .listen(3004, () => console.log("server, listening on port 3004"));
-  
